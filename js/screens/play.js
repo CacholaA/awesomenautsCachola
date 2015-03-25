78244ddd9a7d82cb30ebeb4239111504f84f07ef
aaron cachola <miakelcachola@hotmail.com>
2015-03-24 23:24:36 -0700

    update

--- js/screens/play.js
+++ js/screens/play.js
@@ -5,11 +5,20 @@ game.PlayScreen = me.ScreenObject.extend({
 	onResetEvent: function() {
 		// reset the score
 		game.data.score = 0;
+
                 me.levelDirector.loadLevel("level01");
- 
-                var player = me. pool.pull("player", 0, 420, {} );
+                
+                var player = me.pool.pull("player", 0, 420, {});
                 me.game.world.addChild(player, 5);
-                me.input.bindKey(me.input.KEY.RIGHT, "right");
+                
+                var gamemanager = me.pool.pull("GameManager",0, 0, {});
+                me.game.world.addChild(gamemanager, 0);
+                
+                me.input.bindKey(me.input.KEY.RIGHT,"right");
+                me.input.bindKey(me.input.KEY.LEFT,"left");
+                me.input.bindKey(me.input.KEY.UP, "jump");
+                me.input.bindKey(me.input.KEY.A,"attack");
+                
 		// add our HUD to the game world
 		this.HUD = new game.HUD.Container();
 		me.game.world.addChild(this.HUD);
