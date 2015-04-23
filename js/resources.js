74fc83be75e009866381496ef910a073a750623a
aaron cachola <miakelcachola@hotmail.com>
2015-04-22 21:49:59 -0700

    update

--- js/resources.js
+++ js/resources.js
@@ -6,15 +6,17 @@ game.resources = [
 	 */
         
         {name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
-        {name: "galaxy-wallpapers-20", type:"image", src: "data/img/galaxy-wallpapers-20.jpg"},
         {name: "meta-tiles", type:"image", src: "data/img/meta-tiles.png"},
-        {name: "player", type:"image", src: "data/img/custom2"},
+        {name: "background", type:"image", src: "data/img/background.png"},
+        {name: "player", type:"image", src: "data/img/orcSpear.png"},
         {name: "tower", type:"image", src: "data/img/tower_round.svg.png"},
         {name: "creep1", type:"image", src: "data/img/brainmonster.png"},
-        {name: "lava-screen", type:"image", src: "data/img/lava.png"},
+        {name: "title-screen", type:"image", src: "data/img/title.png"},
+      {name: "galaxy-wallpapers-20", type:"image", src: "data/img/galaxy-wallpapers-20.jpg"},
         {name: "exp-screen", type:"image", src: "data/img/char_select.png"},
         {name: "gold-screen", type:"image", src: "data/img/spend.png"},
-
+        {name: "load-screen", type:"image", src: "data/img/char_select.png"},
+        
 	/* Atlases 
 	 * @example
 	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
@@ -25,9 +27,10 @@ game.resources = [
 	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
 	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
  	 */
-        
-        {name: "level01", type: "tmx", src: "data/map/testmap.tmx"},
 
+         {name: "level01", type: "tmx", src: "data/map/testmap.tmx"},
+        
+         
 	/* Background music. 
 	 * @example
 	 * {name: "example_bgm", type: "audio", src: "data/bgm/"},
